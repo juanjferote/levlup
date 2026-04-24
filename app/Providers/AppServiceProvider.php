4737c8c905da4860dd4790b04713270f34f25062
juanjferote <juanjobabia1997@gmail.com>
2026-04-24 19:01:56 +0200

    deploy: forzar HTTPS en producción

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -6,6 +6,7 @@ use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\View;
 use Illuminate\Support\Facades\Auth;
 use App\Services\NivelService;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -22,6 +23,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        if (env('APP_ENV') === 'production') {
+            URL::forceScheme('https');
+        }
         // hace que $xpPorcentaje esté disponible en todas las vistas
         // que usan el layout principal, sin repetirlo en cada controlador
         View::composer('layouts.main-layout', function ($view) {
