1a8d63b6550c0657a1cae63148a960c42704f8e9
Prácticas Software 3 <juanjobabia1997@gmail.com>
2026-05-13 09:55:01 +0200

    Commit previo a cambios en el diseño responsive

--- resources/views/habitos/index.blade.php
+++ resources/views/habitos/index.blade.php
@@ -4,103 +4,103 @@
 
 @section('contenido')
 
-    <div class="pagina-titulo">
-        <h2>Mis Hábitos</h2>
-        <a href="{{ route('habitos.create') }}" class="btn-primario">+ Nuevo hábito</a>
-    </div>
+<div class="pagina-titulo">
+    <h2>Mis Hábitos</h2>
+    <a href="{{ route('habitos.create') }}" class="btn-primario">+ Nuevo hábito</a>
+</div>
 
-    {{-- pestañas --}}
-    <div class="pestanas">
-        <button class="pestana activa" data-pestana="pendientes">
-            Pendientes
-            @if($habitosHacer->isNotEmpty())
-                <span class="pestana-contador">{{ $habitosHacer->count() }}</span>
-            @endif
-        </button>
-        <button class="pestana" data-pestana="dejar">
-            Dejando atrás
-            @if($habitosDejar->isNotEmpty())
-                <span class="pestana-contador">{{ $habitosDejar->count() }}</span>
-            @endif
-        </button>
-        <button class="pestana" data-pestana="completados-hoy">
-            Completados hoy
-            @if($habitosRegistrados->isNotEmpty())
-                <span class="pestana-contador verde">{{ $habitosRegistrados->count() }}</span>
-            @endif
-        </button>
-        <button class="pestana" data-pestana="objetivo-cumplido">
-            Objetivo cumplido
-            @if($habitosCompletados->isNotEmpty())
-                <span class="pestana-contador verde">{{ $habitosCompletados->count() }}</span>
-            @endif
-        </button>
-        <button class="pestana" data-pestana="archivados">
-            Archivados
-            @if($habitosArchivados->isNotEmpty())
-                <span class="pestana-contador">{{ $habitosArchivados->count() }}</span>
-            @endif
-        </button>
-    </div>
+{{-- pestañas --}}
+<div class="pestanas pestanas--habitos">
+    <button class="pestana activa" data-pestana="pendientes" data-movil="Pend.">
+        Pendientes
+        @if($habitosHacer->isNotEmpty())
+        <span class="pestana-contador">{{ $habitosHacer->count() }}</span>
+        @endif
+    </button>
+    <button class="pestana" data-pestana="dejar" data-movil="Dejar">
+        Dejando atrás
+        @if($habitosDejar->isNotEmpty())
+        <span class="pestana-contador">{{ $habitosDejar->count() }}</span>
+        @endif
+    </button>
+    <button class="pestana" data-pestana="completados-hoy" data-movil="Hoy">
+        Completados hoy
+        @if($habitosRegistrados->isNotEmpty())
+        <span class="pestana-contador verde">{{ $habitosRegistrados->count() }}</span>
+        @endif
+    </button>
+    <button class="pestana" data-pestana="objetivo-cumplido" data-movil="Semana">
+        Objetivo cumplido
+        @if($habitosCompletados->isNotEmpty())
+        <span class="pestana-contador verde">{{ $habitosCompletados->count() }}</span>
+        @endif
+    </button>
+    <button class="pestana" data-pestana="archivados" data-movil="Arch.">
+        Archivados
+        @if($habitosArchivados->isNotEmpty())
+        <span class="pestana-contador">{{ $habitosArchivados->count() }}</span>
+        @endif
+    </button>
+</div>
 
-    {{-- pendientes --}}
-    <div class="pestana-contenido pestana-contenido-grande activo" id="pestana-pendientes">
-        <div class="bloque">
-            <h3 class="bloque-titulo">// Hábitos pendientes</h3>
-            @forelse($habitosHacer as $habito)
-                @include('habitos._habito', ['habito' => $habito, 'completado' => false])
-            @empty
-                <p class="texto-suave">No tienes hábitos pendientes. ¡Buen trabajo!</p>
-            @endforelse
-        </div>
+{{-- pendientes --}}
+<div class="pestana-contenido pestana-contenido-grande activo" id="pestana-pendientes">
+    <div class="bloque">
+        <h3 class="bloque-titulo">// Hábitos pendientes</h3>
+        @forelse($habitosHacer as $habito)
+        @include('habitos._habito', ['habito' => $habito, 'completado' => false])
+        @empty
+        <p class="texto-suave">No tienes hábitos pendientes. ¡Buen trabajo!</p>
+        @endforelse
     </div>
+</div>
 
-    {{-- dejando atrás --}}
-    <div class="pestana-contenido pestana-contenido-grande" id="pestana-dejar">
-        <div class="bloque">
-            <h3 class="bloque-titulo">// Dejando atrás</h3>
-            @forelse($habitosDejar as $habito)
-                @include('habitos._habito', ['habito' => $habito, 'completado' => false])
-            @empty
-                <p class="texto-suave">No tienes hábitos de dejar activos.</p>
-            @endforelse
-        </div>
+{{-- dejando atrás --}}
+<div class="pestana-contenido pestana-contenido-grande" id="pestana-dejar">
+    <div class="bloque">
+        <h3 class="bloque-titulo">// Dejando atrás</h3>
+        @forelse($habitosDejar as $habito)
+        @include('habitos._habito', ['habito' => $habito, 'completado' => false])
+        @empty
+        <p class="texto-suave">No tienes hábitos de dejar activos.</p>
+        @endforelse
     </div>
+</div>
 
-    {{-- completados hoy --}}
-    <div class="pestana-contenido pestana-contenido-grande" id="pestana-completados-hoy">
-        <div class="bloque">
-            <h3 class="bloque-titulo">// Completados hoy</h3>
-            @forelse($habitosRegistrados as $habito)
-                @include('habitos._habito', ['habito' => $habito, 'completado' => true])
-            @empty
-                <p class="texto-suave">Aún no has registrado ningún hábito hoy.</p>
-            @endforelse
-        </div>
+{{-- completados hoy --}}
+<div class="pestana-contenido pestana-contenido-grande" id="pestana-completados-hoy">
+    <div class="bloque">
+        <h3 class="bloque-titulo">// Completados hoy</h3>
+        @forelse($habitosRegistrados as $habito)
+        @include('habitos._habito', ['habito' => $habito, 'completado' => true])
+        @empty
+        <p class="texto-suave">Aún no has registrado ningún hábito hoy.</p>
+        @endforelse
     </div>
+</div>
 
-    {{-- objetivo semanal cumplido --}}
-    <div class="pestana-contenido pestana-contenido-grande" id="pestana-objetivo-cumplido">
-        <div class="bloque">
-            <h3 class="bloque-titulo">// Objetivo semanal cumplido</h3>
-            @forelse($habitosCompletados as $habito)
-                @include('habitos._habito', ['habito' => $habito, 'completado' => true])
-            @empty
-                <p class="texto-suave">Aún no has cumplido el objetivo semanal de ningún hábito.</p>
-            @endforelse
-        </div>
+{{-- objetivo semanal cumplido --}}
+<div class="pestana-contenido pestana-contenido-grande" id="pestana-objetivo-cumplido">
+    <div class="bloque">
+        <h3 class="bloque-titulo">// Objetivo semanal cumplido</h3>
+        @forelse($habitosCompletados as $habito)
+        @include('habitos._habito', ['habito' => $habito, 'completado' => true])
+        @empty
+        <p class="texto-suave">Aún no has cumplido el objetivo semanal de ningún hábito.</p>
+        @endforelse
     </div>
+</div>
 
-    {{-- archivados --}}
-    <div class="pestana-contenido pestana-contenido-grande" id="pestana-archivados">
-        <div class="bloque">
-            <h3 class="bloque-titulo">// Hábitos archivados</h3>
-            @forelse($habitosArchivados as $habito)
-                @include('habitos._habito', ['habito' => $habito, 'completado' => false])
-            @empty
-                <p class="texto-suave">No tienes hábitos archivados.</p>
-            @endforelse
-        </div>
+{{-- archivados --}}
+<div class="pestana-contenido pestana-contenido-grande" id="pestana-archivados">
+    <div class="bloque">
+        <h3 class="bloque-titulo">// Hábitos archivados</h3>
+        @forelse($habitosArchivados as $habito)
+        @include('habitos._habito', ['habito' => $habito, 'completado' => false])
+        @empty
+        <p class="texto-suave">No tienes hábitos archivados.</p>
+        @endforelse
     </div>
+</div>
 
 @endsection
